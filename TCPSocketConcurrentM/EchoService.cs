2325047dc11b0a6a02586ac43e7157e006157710
Zuhair Haroon Khan <zk222ac@gmail.com>
2018-09-17 17:48:21 +0200

    final release

---
+++ TCPSocketConcurrentM/EchoService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPSocketConcurrentM
+{
+    class EchoService
+    {
+        private readonly TcpClient _connectionSocket;
+
+        public EchoService(TcpClient connectionSocket)
+        {
+            // TODO: Complete member initialization
+            this._connectionSocket = connectionSocket;
+        }
+        internal void DoIt()
+        {
+            Stream ns = _connectionSocket.GetStream();
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns) {AutoFlush = true};
+            // enable automatic flushing
+            string message = sr.ReadLine();
+            while (!string.IsNullOrEmpty(message))
+            {
+                Console.WriteLine("Client: " + message);
+                var answer = message.ToUpper();
+                sw.WriteLine(answer);
+                message = sr.ReadLine();
+
+            }
+            ns.Close();
+            _connectionSocket.Close();
+        }
+    }
+}
--- TCPSocketConcurrentM/Program.cs
+++ TCPSocketConcurrentM/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,41 @@ namespace TCPSocketConcurrentM
     {
         static void Main(string[] args)
         {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            TcpListener serverSocket = new TcpListener(ip, 6789);
+
+            //TcpListener serverSocket = new TcpListener(6789);
+            serverSocket.Start();
+
+
+            while (true)
+            {
+                Console.WriteLine("Server is being ready for handshake");
+                TcpClient connectionSocket = serverSocket.AcceptTcpClient();
+                Console.WriteLine("Server activated now");
+                EchoService service = new EchoService(connectionSocket);
+                //Use Task and delegates
+
+                //Task solution with delegates
+                Task.Factory.StartNew(() => service.DoIt());
+
+                //OR
+                //Task.Factory.StartNew(service.DoIt);
+
+                //OR
+                //Task.Run(( ) => service.DoIt());
+
+                //OR
+                //Thread solution
+                //Thread myThread = new Thread(new ThreadStart(service.DoIt));
+
+                //OR
+                //Thread myThread = new Thread(service.DoIt);
+                //myThread.Start();
+
+            }
+
+            serverSocket.Stop();
         }
     }
 }
